0860f601ed8d6dbf7da556af25b6fa47e4434c6b
Mel-iza <meliza.caug@gmail.com>
2023-06-11 22:32:27 -0300

    update changes and files

--- app.py
+++ app.py
@@ -1,37 +1,89 @@
 import streamlit as st
-from utils import PrepProcesor, columns 
-
+import pandas as pd 
+import pickle
+import joblib 
 import numpy as np
-import pandas as pd
-import joblib
+from scipy.sparse import csr_matrix
+import sklearn 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin 
+import re 
+import unicodedata
 
-model = joblib.load('textopadronizado_Naive_Bayes.joblib')
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        if isinstance(X, csr_matrix):
+            X = X.todense()
+        df = pd.DataFrame(X)
+        return df
+
+          
+def clean_text(txt):
+    nfkd = unicodedata.normalize('NFKD', txt)
+    palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+    palavraSemAcento = re.sub(r"(@[A-Za]+)|([^A-Za-z \t])|(\w+:\/\/\S+)| ^rt|http.+?","", palavraSemAcento)
+    palavraSemAcento = str(palavraSemAcento).lower()
+    palavraSemAcento = re.sub(r"\d+", "", palavraSemAcento)
+    palavraSemAcento = re.sub(r'  ', ' ', palavraSemAcento)
+    palavraSemAcento = re.sub(r'compartilheversao para impressao comentarios',' ',
+                                palavraSemAcento)
+    return palavraSemAcento
 
 
-st.title(' Vaccine Fake News Detection 💉🧪')
-header_image ='wallpaper_2.png'
+def preprocess_data(texto):
+    pipeline = Pipeline([
+        ('preprocessor', TextPreprocessor()),
+        ('vectorizer', CountVectorizer(ngram_range=(1, 1))),
+        ('scaler', StandardScaler(with_mean=False))
+    ])
+  
+    # Transforma o texto usando o pipeline
+    vectorized_text = pipeline['vectorizer'].fit_transform([texto])
+    vectorized_text = pipeline['scaler'].fit_transform(vectorized_text)
+    return vectorized_text
+
+
+# Carrega o modelo
+try:
+    model = pickle.load(open('PIPELINE_texto_padronizado_AdaBoost.joblib', 'rb'))
+except Exception as e:
+    st.error(f"Erro ao carregar o modelo: {e}")
+
+
+# Configuração do Streamlit
+header_image ='/home/mel-iza/vaccine_fake_news/src/wallpaper_2.png'
 st.image(header_image, use_column_width=True)
+#st.title("Modelo de Classificação de Texto")
+st.subheader("Detector de notícias falsas sobre vacinação")
+texto = st.text_input("Digite o texto:")
+if st.button("Analisar"):
+    # Limpeza do texto
+    texto_limpo = clean_text(texto)
+    
+    # Pré-processamento e predição
+    predicao = model.predict_proba([texto_limpo])
+    probabilidade = round(np.max(predicao[0]) * 100, 2)
+    
+    # Exibição do resultado
+    st.write(f"Probabilidade: {probabilidade}%")
 
-st.text_input('Digite sua mensagem ou texto sobre vacina/vacinação')
-st.sidebar.header(" ")
-sidebar_information_1 = st.sidebar.selectbox("Fake News about vaccine", 
-("Como saber o que é uma fake new"))
-st.write(sidebar_information_1)
-sidebar_information_2 = st.sidebar.selectbox("About Project", 
-("Tcc", "motivations"))
-st.write(sidebar_information_2)
-sidebar_information_3 = st.sidebar.selectbox("Data", 
-("How data was collected?", "Visualizations"))
-st.write(sidebar_information_3)
+#trigger = st.button('Enviar', on_click=probabilidade)
+#model = joblib.load('/home/mel-iza/vaccine_fake_news/PIPELINE_texto_padronizado_AdaBoost.joblib')
 
 
-def predict(): 
-    row = np.array([text]) 
-    X = pd.DataFrame([row], columns = columns)
-    prediction = model.predict_proba(X)
-    if prediction[0] > 0.50: 
-        st.success('É bem possível que essa informação sobre vacina seja falsa.')
-    else: 
-        st.error('É bem possível que essa informação sobre vacina seja verdadeira') 
+#input_text = clean_text(input_text)
+#predicao = model.predict_proba([input_text])
+#probabilidade = round(np.max(predicao[0]) * 100, 2)
 
-trigger = st.button('Predict', on_click=predict)
+#print(f"Probabilidade: {probabilidade}%")
+
+#preprocessed_text = process_data(input_text)
+#prediction = predict(preprocessed_text, model) 
+
+
